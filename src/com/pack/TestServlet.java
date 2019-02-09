1947cd3ac3112d1366fc84a0d73fa49ee69404a8
HANOK <HANOK@LAPTOP-AQK87RR3>
2019-02-09 12:53:41 +0530

    source code changed

--- src/com/pack/TestServlet.java
+++ src/com/pack/TestServlet.java
@@ -2,6 +2,9 @@ package com.pack;
 
 public class TestServlet {
 public static void main(String[] args) {
-	System.out.println("helloworld");
+	int a=5;
+	int b=6;
+	int c=a+b;
+	System.out.println("c");
 }
 }
